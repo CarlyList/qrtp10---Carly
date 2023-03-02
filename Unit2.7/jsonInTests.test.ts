9f2b2bbb52333f6a6a95bca91e88f6f04943b95f
CarlyList <carlyliston8@gmail.com>
2023-03-01 18:00:50 -0600

    update

---
+++ Unit2.7/jsonInTests.test.ts
@@ -0,0 +1,1 @@
+import * as movies from "insert doc path here" 
--- Unit2.7/setupPractice/employeeManager7.2.test.ts
+++
@@ -1,9 +0,0 @@
-import { SpecPage } from "./employeeManagerPageObjects";
-const chromedriver = require("chromedriver");
-import { WebDriver, Builder, Capabilities } from "selenium-webdriver";
-
-const driver: WebDriver = new Builder()
-  .withCapabilities(Capabilities.chrome())
-  .build();
-
-const page = new SpecPage(driver);
